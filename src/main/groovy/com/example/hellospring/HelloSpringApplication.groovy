cc1e3d725f722ce5798b08f8aa69db20393884d6
José Luis RB <ruizbaenajoseluis@gmail.com>
2021-07-28 11:45:33 +0200

    Version Hello world adatada a hello yusepe sobre el path / y puerto 8084

--- src/main/groovy/com/example/hellospring/HelloSpringApplication.groovy
+++ src/main/groovy/com/example/hellospring/HelloSpringApplication.groovy
@@ -2,12 +2,20 @@ package com.example.hellospring
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@RestController
 class HelloSpringApplication {
 
     static void main(String[] args) {
-        SpringApplication.run(HelloSpringApplication, args)
+        SpringApplication.run(HelloSpringApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "Yusepe :)") String name) {
+        return String.format("Ciao %s!", name);
+    }
 }
